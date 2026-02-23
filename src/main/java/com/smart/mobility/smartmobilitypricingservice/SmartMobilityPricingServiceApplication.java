c0352105b112d7901f5632b46e9fb07b58b6c917
Sidi Ahmed Tidjane Sarr <sats0264@gmail.com>
2026-02-23 11:00:15 +0000

    Initial commit - Pricing Service

---
+++ src/main/java/com/smart/mobility/smartmobilitypricingservice/SmartMobilityPricingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smart.mobility.smartmobilitypricingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMobilityPricingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMobilityPricingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smart/mobility/smartmobilitypricingservice/SmartMobilityPricingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.mobility.smartmobilitypricingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMobilityPricingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
