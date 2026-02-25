9d180cb28e7027f11f96fffa6af38772af9ab7e4
Sidi Ahmed Tidjane Sarr <sats0264@gmail.com>
2026-02-25 20:09:43 +0000

    Add FeignClient for the communication with the userService

--- src/main/java/com/smart/mobility/smartmobilitypricingservice/SmartMobilityPricingServiceApplication.java
+++ src/main/java/com/smart/mobility/smartmobilitypricingservice/SmartMobilityPricingServiceApplication.java
@@ -2,8 +2,10 @@ package com.smart.mobility.smartmobilitypricingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SmartMobilityPricingServiceApplication {
 
     public static void main(String[] args) {
